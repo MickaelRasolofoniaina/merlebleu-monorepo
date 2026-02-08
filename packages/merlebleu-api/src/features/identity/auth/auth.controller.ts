244a2ce17270b513bab689a1bc83b47799159455
Mickael <mickael@stelog.com>
2026-02-08 12:35:19 +0300

    feat(auth): implement authentication guard and skip auth decorator

--- packages/merlebleu-api/src/features/identity/auth/auth.controller.ts
+++ packages/merlebleu-api/src/features/identity/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './auth.dto';
+import { SkipAuth } from 'src/shared/auth/public';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @SkipAuth()
   @HttpCode(HttpStatus.OK)
   @Post('signin')
   async signIn(@Body() body: SignInDto) {
